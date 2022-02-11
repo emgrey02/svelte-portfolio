2e35a41dc33996d9286c6d173906f64f4d63f143
Emma Grey <emgrey02@gmail.com>
2022-02-11 15:23:37 -0500

    Change thumbnail image

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
 		{
 			title: "Harmonic Presence",
 			link: "https://harmonicpresence.netlify.app",
-			image: "https://res.cloudinary.com/greypse/image/upload/c_thumb,w_200,g_face/v1644447893/harm-pres_wmhy4q.png",
+			image: "https://res.cloudinary.com/greypse/image/upload/c_thumb,w_400,g_face/v1644447893/harm-pres_wmhy4q.png",
 			description: "An interactive multimedia presentation experienced through the browser. Made for large devices. Its purpose is to educate and inform anyone who's curious about the existence of harmonics.",
 			technologies: ['Pixi.js', 'Typescript', 'Html/CSS', 'Netlify', 'Webpack'],
 			code: "https://github.com/emgrey02/harmonic-presence-pres"
@@ -35,5 +35,6 @@
 </main>
 
 <style>
+	
 	
 </style>
